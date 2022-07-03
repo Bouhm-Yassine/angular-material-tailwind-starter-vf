119aff325324fec2ad1be836ed1cfad4909b8030
BOUHM Yassine <yassine.bouhm@edu.uiz.ac.ma>
2022-07-03 15:45:42 +0100

    Fisr commit

---
+++ src/app/main/layout/layout-side-nav/layout-side-nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'layout-side-nav',
+  templateUrl: './layout-side-nav.component.html',
+  styleUrls: ['./layout-side-nav.component.scss']
+})
+export class LayoutSideNavComponent implements OnInit {
+  @ViewChild('sidenavMain') sidenavMain!: ElementRef;
+  @ViewChild('logoImg') logoImg!: ElementRef;
+  isOpen: boolean = true;
+  showSubAdmin: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleSidenav() {
+    this.isOpen = !this.isOpen;
+
+    if (this.isOpen) {
+      this.sidenavMain.nativeElement.classList.remove('close-nav-state');
+      this.sidenavMain.nativeElement.classList.add('open-nav-state');
+      this.sidenavMain.nativeElement.style = '';
+
+      this.logoImg.nativeElement.style = '';
+    } else {
+      this.sidenavMain.nativeElement.classList.remove('open-nav-state');
+      this.sidenavMain.nativeElement.classList.add('close-nav-state');
+      this.sidenavMain.nativeElement.style.width = '64px';
+      this.logoImg.nativeElement.style.display = 'none';
+    }
+  }
+}
